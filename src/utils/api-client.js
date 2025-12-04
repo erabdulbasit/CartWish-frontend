62cd1cbdb7b0abdf21d8d24ac7493cf0680339a3
AbdulBasit <108606170+erabdulbasit@users.noreply.github.com>
2025-12-04 15:38:32 +0530

    refactor: switched to environment variables for backend URL

--- src/utils/api-client.js
+++ src/utils/api-client.js
@@ -1,6 +1,11 @@
 import axios from "axios";
-import config from "../config.json";
+// import config from "../config.json";
+
+// Access the variable using Vite's special object
+const backendURL = import.meta.env.VITE_API_URL;
 
 export default axios.create({
-  baseURL: `${config.backendURL}/api`,
+  //baseURL: `${config.backendURL}/api`,
+  // If using Vite:
+  baseURL: `${backendURL}/api`,
 });
